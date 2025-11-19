f74759222f9fceb2cdecd36da80038b06e9a1620
plant <alperensahin442@gmail.com>
2025-11-19 20:34:01 +0300

    move favicon and remove redundant files

--- app/root.tsx
+++ app/root.tsx
@@ -26,8 +26,8 @@ export const links: Route.LinksFunction = () => [
 		rel: "stylesheet",
 		href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
 	},
-	{ rel: "icon", href: "/assets/favicon.ico" },
-	{ rel: "apple-touch-icon", href: "/assets/apple-touch-icon.png" },
+	{ rel: "icon", href: "/favicon.ico" },
+	{ rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
